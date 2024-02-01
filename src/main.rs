598938a6988add2a0094172e8a0abd1dff78cc31
Misty De Méo <misty@axo.dev>
2024-02-01 11:53:33 -0800

    0.2.97

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@
 use clap::Parser;
 use unicode_width::UnicodeWidthStr;
 
-use cue::cd::CD;
-
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
@@ -11,15 +9,6 @@ struct Args {
 }
 
 fn main() {
-    CD::parse(
-        r#"FILE "basic_image.bin" BINARY
-          TRACK 01 MODE1/2352
-            INDEX 01 00:00:00
-    "#
-        .to_owned(),
-    )
-    .unwrap();
-
     let args = Args::parse();
     let msg = &args.msg;
     let count = UnicodeWidthStr::width(args.msg.as_str());
